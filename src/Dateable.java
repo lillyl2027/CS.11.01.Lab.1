963c560bd43877db92d9abfe597f590699cce13d
lillyl2027 <lillyl2027@student.cis.edu.hk>
2024-08-23 12:59:36 +0800

    hi

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("What's your age? ");
+        int age = scanner.nextInt();
+        System.out.println(age + "-year olds should date somebody who is at least " + (age / 2 + 7) + " years old.");
     }
+
+
 }
+
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
 
+        //Credit: Thanks to Jim Cohoon at The University of Virginia
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("What's your age? ");
+        int age = scanner.nextInt();
+        System.out.println(age + "-year olds should read at least " + (100-age) + " pages before giving up on a book.");
     }
 }
